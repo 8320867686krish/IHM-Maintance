d1f22117c1ca8f8648be786db7aad6c51eefde85
krishna <krishna.patel@meetanshi.com>
2024-12-18 11:01:13 +0530

    add back btn

--- resources/views/training/add.blade.php
+++ resources/views/training/add.blade.php
@@ -11,16 +11,13 @@
     <div class="row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
             <div class="page-header">
-                <h2 class="pageheader-title">Training Management</h2>
-                {{-- <div class="page-breadcrumb">
+
+                 <div class="page-breadcrumb">
                         <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('training') }}" class="breadcrumb-link">training</a>
-                </li>
-                <li class="breadcrumb-item active"><a href="#" class="breadcrumb-link">{{ $head_title ?? 'Add' }}</a></li>
-                </ol>
+                        <h5 class="pageheader-title"> <a href="{{url('training')}}"><span class="icon"><i class="fas fa-arrow-left"></i></span> Back</a> <span class="ml-1">{{ $head_title ?? '' }} Training Management</span></h5>
+
                 </nav>
-            </div> --}}
+            </div> 
         </div>
     </div>
 </div>
