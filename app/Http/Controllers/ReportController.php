1704381291208ca1abe6035d91fc98eb74162a08
krishna <krishna.patel@meetanshi.com>
2025-03-03 10:10:21 +0530

    now show page number

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -262,8 +262,9 @@ class ReportController extends Controller
                 ($mpdf->w - $mpdf->lMargin - $mpdf->rMargin) / $size['width'],
                 ($mpdf->h - $mpdf->tMargin - $mpdf->bMargin) / $size['height']
             );
-            if ($i === 1 && @$title) {
-                $mpdf->WriteHTML($title);
+            if ($i === 1 && !empty($title)) {
+                $mpdf->WriteHTML($title, \Mpdf\HTMLParserMode::HTML_BODY);
+
                 $lmargin = 10;
                 $tMargin = 20;
             } else {
