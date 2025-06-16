411b11ed9dcd770efda4565cd0dad6599eb962bd
krishna <krishna.patel@meetanshi.com>
2025-06-16 19:03:11 +0530

    set 400

--- app/Http/Controllers/ShipController.php
+++ app/Http/Controllers/ShipController.php
@@ -167,7 +167,7 @@ class ShipController extends Controller
                 }
                 $imageFile = $request->file('ship_image');
                 $imageName = time() . '.' . $imageFile->getClientOriginalExtension();
-                $targetSize = 250;
+                $targetSize = 400;
 
                 // Get original dimensions
                 list($width, $height) = getimagesize($imageFile);
