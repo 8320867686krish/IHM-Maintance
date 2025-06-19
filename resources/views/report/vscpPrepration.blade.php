24d2ab601b437df5d3a0d868e1a35b0e6bb13bba
krishna <krishna.patel@meetanshi.com>
2025-06-19 14:47:38 +0530

    set proper image

--- resources/views/report/vscpPrepration.blade.php
+++ resources/views/report/vscpPrepration.blade.php
@@ -14,7 +14,22 @@
         width: 100%;
         margin-bottom: 40px;
         border-collapse: collapse;
-        margin-top:20px;
+        margin-top: 20px;
+    }
+
+    .section-1-1 .img-cell img {
+        width: 100%;
+        /* Fill the cell */
+        height: auto;
+        /* Keep aspect ratio */
+        max-height: 200px;
+        /* Prevent images from being too tall */
+        object-fit: cover;
+        /* Crop to fit */
+        display: block;
+        /* Remove inline spacing */
+        margin: 0 auto;
+        /* Center the image */
     }
 
     .section-1-1 th,
@@ -51,21 +66,21 @@
 
         <tbody>
             <tr>
-               
+
 
                 <td><strong>Check Point ID</strong></td>
                 <td>{{ $check->check->name }}</td>
 
-                <td><strong>Check Point  Type</strong></td>
+                <td><strong>Check Point Type</strong></td>
                 <td>{{ $check->check->type }}</td>
 
 
 
             </tr>
             <tr>
-              <td><strong>Hazma Statust</strong></td>
+                <td><strong>Hazma Statust</strong></td>
                 <td>{{ $check->hazmat_type ?? '' }}</td>
-                
+
                 <td><strong>Hazmat</strong></td>
                 <td>{{ $check->hazmat->name ?? '' }}</td>
 
@@ -90,10 +105,10 @@
                 <td><strong>Quantity</strong></td>
                 <td>{{ $check->qty }}</td>
 
-                 <td><strong>Unit</strong></td>
+                <td><strong>Unit</strong></td>
                 <td>{{ $check->unit }}</td>
             </tr>
-           
+
 
             <tr>
                 <td colspan="4"><strong>Remarks: </strong>{{ $check->remarks }}</td>
@@ -103,25 +118,21 @@
                 <td colspan="2" width="50%"><strong>CheckPoint Image 2:</strong></td>
             </tr>
             <tr>
-                <td colspan="2" width="50%" class="img-cell">
-                    @if($check->check->close_image)
-                    <img src="{{ $check->check->close_image }}"
-                        alt="Close Image"
-                        style="width: 40%;height: 40%;">
-                    @else
-                    <span>No image available</span>
-                    @endif
-                </td>
-                <td colspan="2" width="50%" class="img-cell">
-                    @if($check->check->away_image)
-                    <img src="{{ $check->check->away_image }}"
-                        alt="Close Image"
-                        style="width: 40%;height:40%;">
-                    @else
-                    <span>No image available</span>
-                    @endif
-                </td>
-
+              <td colspan="2" width="50%" class="img-cell">
+    @if($check->check->close_image)
+        <img src="{{ $check->check->close_image }}" alt="Close Image">
+    @else
+        <span>No image available</span>
+    @endif
+</td>
+<td colspan="2" width="50%" class="img-cell">
+    @if($check->check->away_image)
+        <img src="{{ $check->check->away_image }}" alt="Close Image">
+    @else
+        <span>No image available</span>
+    @endif
+</td>
+                
             </tr>
         </tbody>
     </table>
