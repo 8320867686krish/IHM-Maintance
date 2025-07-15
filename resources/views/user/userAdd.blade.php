7cf54d39582139535898759e91fda3a7ac6dd541
Akash3193 <141951700+Akash3193@users.noreply.github.com>
2025-07-15 16:55:38 +0530

    changes for passowrd

--- resources/views/user/userAdd.blade.php
+++ resources/views/user/userAdd.blade.php
@@ -99,8 +99,7 @@
                         <div class="col-sm-12 col-md-4">
                             <div class="form-group input-label-group">
                                 <label for="password">Password <span class="text-danger">*</span></label>
-                               <input type="password" class="form-control" id="password" name="password" autocomplete="off">
-
+<input type="password" class="form-control " id="password" name="password" value="" placeholder="" autocomplete="off" onchange="removeInvalidClass(this)">
                                 <div class="invalid-feedback error" id="passwordError"></div>
                             </div>
                         </div>
