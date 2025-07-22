e7de2cef55453f1163b364217afcca58bae19e39
Akash3193 <141951700+Akash3193@users.noreply.github.com>
2025-07-22 15:34:23 +0530

    poorder history

--- app/Http/Controllers/POOrderController.php
+++ app/Http/Controllers/POOrderController.php
@@ -42,10 +42,10 @@ class POOrderController extends Controller
         }
         $backurl = "ship/view" . "/" . $ship_id . "#po-records";
         $poData = poOrder::with('poOrderItems', 'emailHistory')->find($po_order_id);
-        $filteredEmailHistory = $poData->emailHistory->filter(function ($history) {
+        $filteredEmailHistory = @$poData->emailHistory->filter(function ($history) {
             return $history->history_type == 'ship';
         });
-        $filteredEmailHVendoristory = $poData->emailHistory->filter(function ($history) {
+        $filteredEmailHVendoristory = @$poData->emailHistory->filter(function ($history) {
             return $history->history_type == 'vendor';
         });
         return view('ships.po.add', compact('head_title', 'ship_id', 'poData', 'backurl', 'client_name', 'currentUserRoleLevel', 'filteredEmailHVendoristory', 'filteredEmailHistory'));
